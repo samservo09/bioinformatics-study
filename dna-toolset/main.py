e261a25e5a37e3e6c5a6fbe45f756756885eb92f
Vivien <samanthaservo09@gmail.com>
2024-09-06 00:48:39 +0800

    fixed results display

--- dna-toolset/main.py
+++ dna-toolset/main.py
@@ -6,7 +6,9 @@ randDNAstr = ''.join([random.choice(nucleotides)
                       for nuc in range(50)])
 
 DNAstr = validateDNAseq(randDNAstr)
-print(countNucFreq(DNAstr))
 
-def transcription(seq):
-    return seq.replace("T", "U") 
+# print out results
+print(f"\nSequence: {DNAstr}\n")
+print(f"[1] + Sequence Length: {len(DNAstr)}\n")
+print(f"[2] + Nucleotide Frequency: {countNucFreq(DNAstr)}\n")
+print(f"[3] + DNA/RNA Transcription: {transcription(DNAstr)}\n")
